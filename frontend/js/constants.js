7e15d5207fe5816cb9e68d5f31fb9077492c1f85
smackd40 <mdawson40@gmail.com>
2022-04-11 17:14:51 -0500

    Text updates

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Music Dog NFT Project!";
+const welcome_h1 = "Music Dog NFTs";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The Music Dog NFT Project is an Artist Distribution and Promotion platform built exclusively for members.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!";
